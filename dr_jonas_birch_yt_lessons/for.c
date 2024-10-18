297d89a6f0c7a6802041c4778c9e6d0c5b18da18
Abraham Garcia <garcia.abraham@comcast.net>
2024-10-17 20:30:07 -0500

    Lesson 7: dr jonas birch c programming
    learned about define to make an alias, macro, and use assert to check conditions and for loop

---
+++ dr_jonas_birch_yt_lessons/for.c
@@ -0,0 +1,33 @@
+/* for.c */
+#include <stdio.h>
+#include <assert.h>
+
+#define F fflush(stdout)
+
+void multi(int t, int v)
+{
+
+	int result;
+
+	result = t * v;
+	printf("%d X %d = %d\n",
+		t, v, result);
+	
+	return;
+
+}
+
+int main ()
+{
+	int x, table;
+
+	printf("Select multiplication table: "); F;
+	scanf("%d", &table);
+	assert( (table < 13) && (table > 0));
+
+	for (x = 0; x < 13; x++)
+		multi(table, x);
+
+	return 0;
+
+}
